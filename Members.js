b30d9717f620ad325139e4542d9fac59a543f99a
Johnnatan Ruiz <johnnyruyzo@gmail.com>
2021-01-08 18:06:13 -0500

    Beggining of the proyect Breaking News Backend.

---
+++ Members.js
@@ -0,0 +1,16 @@
+const members = [
+    {
+        id: 1,
+        name: 'John'
+    },
+    {
+        id: 2,
+        name: 'Gloria'
+    },
+    {
+        id: 3,
+        name: 'Johnny'
+    },
+];
+
+module.exports = members;
---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const members = require('./Members');
+const logger = require('./middleware/logger');
+const fetch = require('node-fetch');
+
+const app = express();
+
+//Init middleware
+app.use(logger);
+
+// Gets All News
+app.get('/GetAllNews?:parameters', (req, res) => {
+    let searchTerm = req.originalUrl.replace('/GetAllNews?','');
+    console.log(searchTerm);
+    console.log("https://newsapi.org/v2/everything?" + searchTerm + "&sortBy=publishedAt&apiKey=def59b2c8beb4495896b7bd46a19ca4a");
+    fetch("https://newsapi.org/v2/everything?" + searchTerm + "&sortBy=publishedAt&apiKey=def59b2c8beb4495896b7bd46a19ca4a", {
+          "method": "GET"
+        })
+        .then(response => response.json())
+        .then(response => {
+            res.send(response);
+        })
+        .catch(err => { console.log(err); 
+        });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
